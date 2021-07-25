6b4399bc5a8a4bb6d261e0a583e3353aacbb5464
urnotmargot <271302106@qq.com>
2021-07-25 22:51:46 +0800

    s1

---
+++ Test1.py
@@ -0,0 +1,17 @@
+#2021/07/18
+#selenium安装成功测试
+
+from selenium import webdriver
+
+#填写chromedriver安装地址
+
+driver=webdriver.Chrome("D:\webDriver\chromedriver_win32\chromedriver.exe")
+#填写需测试的网页地址
+
+driver.get("https://www.jianshu.com/u/a2d1bc176e90")
+
+#打开窗口最大化
+
+driver.maximize_window()
+
+
